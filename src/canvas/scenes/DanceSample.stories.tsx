06d86737ef15d105a8a9b7e9c5ef4f2fb7773c1f
sbleru <usareru@gmail.com>
2022-09-11 03:08:20 +0900

    feat: Add storybook for vite
    see https://github.com/storybookjs/builder-vite

---
+++ src/canvas/scenes/DanceSample.stories.tsx
@@ -0,0 +1,24 @@
+import { ComponentProps } from "react";
+import { DanceSample } from "./DanceSample";
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { Center } from "@chakra-ui/react";
+
+type Meta = ComponentMeta<typeof DanceSample>;
+type Props = ComponentProps<typeof DanceSample>;
+type Story = ComponentStoryObj<typeof DanceSample>;
+
+const componentMeta: Meta = {
+  component: DanceSample,
+};
+export default componentMeta;
+
+const render = (_props: Props) => (
+  <Center h={"100vh"}>
+    <DanceSample />
+  </Center>
+);
+const Template: Story = { render };
+
+export const Default: Story = {
+  ...Template,
+};
